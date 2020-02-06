47b5dbba5252288c3435c04d07902e1041848301
Subhannita Sarcar <subhannitasarcar1994@gmail.com>
2020-02-05 19:49:50 -0500

    initial commit

---
+++ private/initialize.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+
+$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+define("WWW_ROOT", $doc_root);
+
+require_once("functions.php");
+
+?>

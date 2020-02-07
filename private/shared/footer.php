2117eb8182a92cd4ccabc71f3f259a1e533d12b1
Subhannita Sarcar <subhannitasarcar1994@gmail.com>
2020-02-07 03:56:29 -0500

    #4gaevb

---
+++ private/shared/footer.php
@@ -0,0 +1,22 @@
+<!-- Footer -->
+  <footer class="bg-black">
+    <nav class="navbar navbar-expand-sm navbar-dark navbar-custom fixed-bottom">
+    	<p class="m-0 text-left text-white small">&copy; <?php echo COMPANY_NAME . date('Y'); ?></p>
+    	<ul class="navbar-nav ml-auto">
+    		<li class="nav-item">
+    			<a class="nav-link" href="#">Terms and Privacy</a>
+    		</li>
+    		<li class="nav-item">
+    			<a class="nav-link" href="#">Sitemap</a>
+    		</li>
+    		<li class="nav-item">
+    			<a class="nav-link" href="#">Contact us</a>
+    		</li>
+    	</ul>
+
+    </nav>
+  </footer>
+
+  </body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../private/initialize.php');
+$page_title = COMPANY_NAME;
+include(SHARED_PATH . '/header.php');
+
+?>
+
+<header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <h1 class="masthead-heading mb-0">You leave the ride. The ride never leaves you.</h1>
+        <h1 class="masthead-subheading mb-0">#ComeRent</h1>
+        <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+      </div>
+    </div>
+  </header>
+
+
+  <?php
+  include(SHARED_PATH . "/footer.php");
+  ?>
